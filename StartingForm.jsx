018c8225af59cb3f563b6d5ced5e41554f75b301
Andrei <Alfquader@gmail.com>
2025-09-10 21:25:52 +0300

    PopChoiceV3

--- StartingForm.jsx
+++ StartingForm.jsx
@@ -16,15 +16,36 @@ export default function StartingForm(props) {
         <>
             <form action={startApp}>
                 
-                <input type="number" className="input starting" name="people-number" placeholder="How mane people?" required></input>
+                <input 
+                    type="number" 
+                    className="input starting" 
+                    name="people-number" 
+                    placeholder="How mane people?" 
+                    defaultValue="5"
+                    required
+                ></input>
                 
                 <label>How much time do you have?</label>
                 <div className="input starting time-container">
 
-                    <input className="time-input" type="number" name="hours-amount" placeholder="" required></input>
+                    <input 
+                        className="time-input" 
+                        type="number" 
+                        name="hours-amount" 
+                        placeholder="" 
+                        defaultValue="1"
+                        required
+                    ></input>
                     <span>hours</span>
 
-                    <input className="time-input" type="number" name="minutes-amount" placeholder="" required></input>
+                    <input 
+                        className="time-input" 
+                        type="number" 
+                        name="minutes-amount" 
+                        placeholder="" 
+                        defaultValue="20"
+                        required
+                    ></input>
                     <span>minutes</span>
 
                 </div>
