7b88350952898acdf08e85befb6e7330a5667dd7
Andrei <Alfquader@gmail.com>
2025-09-13 20:16:53 +0300

    PopChoiceV5

--- Recommendation.jsx
+++ Recommendation.jsx
@@ -5,6 +5,7 @@ export default function Recommendation(props) {
     return (
         <div className="recommendation-container">
             <p className="recommendation-title">{`${props.recommendation.title} (${props.recommendation.releaseYear})`}</p> 
+            <img className="poster" src={props.recommendation.posterUrl}></img>
             <p className="recommendation-description">{props.recommendation.response}</p> 
             <button onClick={props.resetApp}>Go Again</button>
         </div>
